2f925c01f90c3cbc11a93f4f5f3007e523c2ef4e
Scott Wallace <ecosse_18@hotmail.com>
2014-10-12 18:31:02 -0400

    openBankingSystem function

--- CISC327Asgn2.py
+++ CISC327Asgn2.py
@@ -15,6 +15,8 @@ class retail(object):
 		return 0 
 	def transfer():
 		return 0 
+	def runRetailDay():
+		return 0	
 
 class agent(object):
 	def __init__(self, type):
@@ -29,3 +31,32 @@ class agent(object):
 		return 0 
 	def delete():
 		return 0 
+	def runAgentDay():
+		return 0	
+
+def openBankingSystem():
+	loggedIn = true
+	while (loggedIn):
+		firstInput = raw_input('Type "login" to login: ')
+		firstInput.lowercase
+		if (firstInput = "login"):
+			pickDay = true
+			while (pickDay)
+				dayType = raw_input('agent or retail: ')
+				dayType.lowercase
+				if (dayType = "retail"):
+					pickDay = false
+					retailDay = retail(dayType)
+					loggedIn = retailDay.runRetailDay()
+				else if (dayType = "agent"):
+					pickDay = false	
+					agentDay = agent(dayType)
+					loggedIn = agentObject.runAgentDay()
+				else: 
+					print "Please enter a valid input.\n"
+		else:
+			print "Please enter a valid input.\n"
+	return openBankingSystem()			
+
+######	 MAIN PROGRAM 	######
+openBankingSystem()
