755193b9cdace27cac3875b5f6e324dec5a6d4c8
Scott Wallace <ecosse_18@hotmail.com>
2014-10-12 18:18:12 -0400

    started adapting pseudo

---
+++ CISC327Asgn2.py
@@ -0,0 +1,31 @@
+"""
+CISC 327
+Breaking Bank
+Assignment #2
+Scott Wallace 10051890
+Brad Guner 10059112
+"""
+
+class retail(object):
+	def __init__(self, type):
+		self.type = type
+	def withdraw():
+		return 0 
+	def deposit():
+		return 0 
+	def transfer():
+		return 0 
+
+class agent(object):
+	def __init__(self, type):
+		self.type = type
+	def withdraw():
+		return 0 
+	def deposit():
+		return 0 
+	def transfer():
+		return 0 
+	def create():
+		return 0 
+	def delete():
+		return 0 
