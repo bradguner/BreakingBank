685be66b7b46694eab2ff9ca5d01a08433e9e53b
Scott Wallace <ecosse_18@hotmail.com>
2014-10-14 17:35:10 -0400

    full system update

--- CISC327Asgn2.py
+++ CISC327Asgn2.py
@@ -211,7 +211,7 @@ class agent(object):
 							amount = amount*100
 							if (amount > 999999):
 								print "Please enter a valid transfer amount."
-							elif (amount > 999999):
+							elif (amount < 0):
 								print "Please enter a valid transfer amount."	
 							else:
 								amt = False
@@ -231,40 +231,40 @@ class agent(object):
 		accNameInput = True
 		while (accNumInput):
 			accNum = int(input('Enter your desired account number: '))
-			#if (accNum < 1000000 && accNum >= 0):	#Account Number must be 6 digits. Maximum of 999999, so if < 1000000, account number is 6 digits long
-			#	#CHECK TO SEE IF INPUT ACCOUNT NUMBER DOES NOT EXIST
-				if (1 == 1):
-					accNumInput = False
-					while (accNameInput): 
-						accName = str(input('Enter your desired account name: '))
-						if (accName.Length > 15 || accName.Length == 0):
-							print "Please enter a valid account name."
-						else:
-							accNameInput = False
-							#create string for write file
-							accNum = str(accNum)
-							accName = str(accName)
-							transactionInfo = '04_' + accNum + "_" + accName #proper formatting on end of string is needed
-				else:
-					print "Please enter a valid account number."
-			#else:
-			#	print "Please anter a valid account number."
+			#Account Number must be 6 digits. Maximum of 999999, so if < 1000000, account number is 6 digits long
+			#CHECK TO SEE IF INPUT ACCOUNT NUMBER DOES NOT EXIST
+			if (1 == 1):
+				accNumInput = False
+				while (accNameInput): 
+					accName = raw_input('Enter your desired account name: ')
+					if (len(accName) > 15 | len(accName) == 0):
+						print "Please enter a valid account name."
+					else:
+						#create account number here
+						accNameInput = False
+						#create string for write file
+						accNum = str(accNum)
+						accName = str(accName)
+						transactionInfo = '04_' + accNum + "_" + accName #proper formatting on end of string is needed
+			else:
+				print "Please enter a valid account number."
 		return transactionInfo
 
 	def delete(self):
 		accNumInput = True
 		accNameInput = True
 		while (accNumInput):
-			accNum = int(input('Enter your account number: '))
+			accNum = int(input('Enter the account number: '))
 			#CHECK TO SEE IF INPUT ACCOUNT NUMBER EXISTS
 			if (1 == 1):
 				accNumInput = False
 				while (accNameInput):
-					accName = str(input('Enter your desired account name: '))
+					accName = raw_input('Enter the account name: ')
 					#CHECK TO SEE IF INPUT ACCOUNT NAME MATCHES ACCOUNT NUMBER
 					if (1 == 0):
 						print "Please enter the proper account name for this account."
 					else:
+						#delete account now
 						accNameInput = False
 						#create string for write file
 						accNum = str(accNum)
