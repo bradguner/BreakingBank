894c212f24ce457a4cf6f4fc6ea1f061db72e579
Scott Wallace <ecosse_18@hotmail.com>
2014-11-03 16:00:24 -0500

    backend

--- breakingbank-backend.py
+++ breakingbank-backend.py
@@ -17,9 +17,25 @@ for each line perform said transaciton on master accounts
 
 constraints
 no negative balances
-deleted account has zero balance, dpes it disappear
+deleted account has zero balance, dpes it disappear?
 created account should not have existing acc num,, 2 create same number
 nam egiven for delete myst have a mathcing name
 
 constraint fails and the fatal error stop
 """
+def transaction(trans):
+	return 0
+
+def writeNewMasterAccounts(list):
+	return 0
+
+def writeNewValidAccounts(list):
+	return 0
+
+def throwError():
+	return 0
+
+def main_program():
+	return 0
+
+main_program()
